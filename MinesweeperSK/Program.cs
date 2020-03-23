95d18674a52eaeaf745e6800ad4d77b8c027ef19
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-03-22 17:15:11 -0700

    created base 9x9 gameboard

--- MinesweeperSK/Program.cs
+++ MinesweeperSK/Program.cs
@@ -7,7 +7,7 @@ namespace MinesweeperSK
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Gameboard.PrintTheBoard();
         }
     }
 }
--- MinesweeperSK/Visuals/Gameboard.cs
+++ MinesweeperSK/Visuals/Gameboard.cs
@@ -4,8 +4,28 @@ namespace MinesweeperSK.Visuals
 {
     public class Gameboard
     {
-        public Gameboard()
+        public static void PrintTheBoard()
         {
+            Console.WriteLine(@"    a   b   c   d   e   f   g   h   i  ");
+            Console.WriteLine(@"   ------------------------------------");
+            Console.WriteLine(@"1 | * | * | * | * | * | * | * | * | * |");
+            Console.WriteLine(@"   ------------------------------------");
+            Console.WriteLine(@"2 | * | * | * | * | * | * | * | * | * |");
+            Console.WriteLine(@"   ------------------------------------");
+            Console.WriteLine(@"3 | * | * | * | * | * | * | * | * | * |");
+            Console.WriteLine(@"   ------------------------------------");
+            Console.WriteLine(@"4 | * | * | * | * | * | * | * | * | * |");
+            Console.WriteLine(@"   ------------------------------------");
+            Console.WriteLine(@"5 | * | * | * | * | * | * | * | * | * |");
+            Console.WriteLine(@"   ------------------------------------");
+            Console.WriteLine(@"6 | * | * | * | * | * | * | * | * | * |");
+            Console.WriteLine(@"   ------------------------------------");
+            Console.WriteLine(@"7 | * | * | * | * | * | * | * | * | * |");
+            Console.WriteLine(@"   ------------------------------------");
+            Console.WriteLine(@"8 | * | * | * | * | * | * | * | * | * |");
+            Console.WriteLine(@"   ------------------------------------");
+            Console.WriteLine(@"9 | * | * | * | * | * | * | * | * | * |");
+            Console.WriteLine(@"   ------------------------------------");
         }
     }
 }
